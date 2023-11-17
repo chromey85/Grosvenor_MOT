91c0f77940838fc4659db567c52c562a4d62690e
Yusuf Ayyub <yayyub85@gmail.com>
2023-11-17 18:13:01 +0000

    added media queries and hamburger menu with JS but logo is misbehaving

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -1,8 +1,26 @@
-fetch("/pages/nav.html")
-  .then((res) => res.text())
-  .then((text) => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem, oldelem);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/pages/nav.html")
+    .then((res) => res.text())
+    .then((text) => {
+      let oldelem = document.querySelector("script#replace_with_navbar");
+      let newelem = document.createElement("div");
+      newelem.innerHTML = text;
+      oldelem.parentNode.replaceChild(newelem, oldelem);
+
+      var menuToggle = document.getElementById("mobile-menu");
+      var nav = document.querySelector(".header_nav_links");
+
+      menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("open");
+      });
+      var conSocial = document.querySelector(".header_logo");
+      var header = document.querySelector(".header");
+
+      if (window.matchMedia("(max-width: 930px)").matches) {
+        header.appendChild(conSocial);
+      } else {
+        var navContainer = document.querySelector(".navContainer");
+        navContainer.appendChild(conSocial);
+      }
+    });
+});
